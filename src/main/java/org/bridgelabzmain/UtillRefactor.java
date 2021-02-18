0e857e980377fd2bf9e15f62198470c76f54bb05
aayush-aryan <MY_NAME@example.com>
2021-02-19 03:35:02 +0530

    [aayush]refactored. a program to test google page by using TestNg and selenium

--- src/main/java/org/bridgelabzmain/UtillRefactor.java
+++ src/main/java/org/bridgelabzmain/UtillRefactor.java
@@ -8,9 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class UtillRefactor extends Base {
-   //static WebDriver driver;
     public static void takeScreenshots(String fileName) throws IOException,NullPointerException {
-        //driver.get("https://www.google.com/");
         File file=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(file,new File( "C:\\Users\\ankit\\Desktop\\testngGoogleSeleniumTest" +
                 "\\src\\main\\java\\org\\bridgelabzmain"+ fileName + ".jpg"));
